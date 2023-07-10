f77c0ad4af1b4ffa21da0cb7a770d4ca1217a2dc
Miguel Mendes Serrano <mgl.mendes21@gmail.com>
2023-07-10 11:52:30 -0300

    Aula 21

---
+++ mundo_3/aula_21/ex104.py
@@ -0,0 +1,18 @@
+# Validando entrada de dados em Python
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[31mERRO! Digite um número válido.\033[m')
+        if ok:
+            break
+    return valor
+
+
+n = leiaInt('Digite um número: ')
+print(f'Você digitou o número {n}')
---
+++ mundo_3/aula_21/ex105.py
@@ -0,0 +1,26 @@
+# Analisando e gerando Dicionários
+def notas(*n, sit=False):
+    """
+    -> Função para analisar notas e situações de vários alunos.
+    :param n: uma ou mais notas dos alunos (aceita várias).
+    :param sit: valor opcional, indicando se deve ou não adicionar a situação.
+    :return: dicionário com várias informações sobre a situação de turma.
+    """
+    r = dict()
+    r['total'] = len(n)
+    r['maior'] = max(n)
+    r['menor'] = min(n)
+    r['média'] = (sum(n) / len(n)).__round__(1)
+    if sit:
+        if r['média'] >= 7:
+            r['situação'] = 'BOA'
+        elif r['média'] >= 5:
+            r['situação'] = 'RAZOÁVEL'
+        else:
+            r['situação'] = 'RUIM'
+    return r
+
+
+resp = notas(10, 5.5, 7, 5.5, sit=True)
+print(resp)
+help(notas)
